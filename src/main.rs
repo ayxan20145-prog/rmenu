74a5b73dd2724cd8ed9d3638fc73e9134f103465
ayxan <ayxan.20145@gmail.com>
2026-07-25 10:22:29 +0400

    draw bar

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
-use x11rb::{connect, connection::Connection};
+use x11rb::{COPY_FROM_PARENT, connect, connection::Connection, protocol::xproto::*};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (conn, screen_num) = connect(None)?;
     let screen = &conn.setup().roots[screen_num];
 
-    println!("{}", screen.root);
+    let window = conn.generate_id()?;
 
-    Ok(())
+    let aux = CreateWindowAux::new()
+        .background_pixel(screen.black_pixel)
+        .override_redirect(1)
+        .event_mask(EventMask::EXPOSURE | EventMask::KEY_PRESS);
+
+    conn.create_window(
+        COPY_FROM_PARENT as u8,
+        window,
+        screen.root,
+        0,
+        0,
+        screen.width_in_pixels,
+        28,
+        0,
+        WindowClass::INPUT_OUTPUT,
+        0,
+        &aux,
+    )?;
+
+    conn.map_window(window)?;
+    conn.flush()?;
+
+    loop {
+        let event = conn.wait_for_event()?;
+
+        match event {
+            x11rb::protocol::Event::Expose(_) => {
+                conn.map_window(window)?;
+                conn.flush()?;
+            }
+            _ => {}
+        }
+    }
 }
